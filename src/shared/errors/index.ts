aefc2841eba63c24bc1630dcea90878682cefd0e
RKRafaelNascimento <36175807+RKRafaelNascimento@users.noreply.github.com>
2025-02-18 00:35:32 -0300

    feat: add UnauthorizedError class for handling authentication errors

--- src/shared/errors/index.ts
+++ src/shared/errors/index.ts
@@ -1,3 +1,4 @@
 export { HttpError } from "./HttpError";
 export { BadRequestError } from "./BadRequestError";
 export { InternalServerError } from "./InternalServerError";
+export { UnauthorizedError } from "./UnauthorizedError";
